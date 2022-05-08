6386f96fccd4dada50a6889981508c86a84dbfcc
PugtgYosuky <joanasimoes04@gmail.com>
2022-05-08 15:07:55 +0100

    First commit

---
+++ src/main/java/com/example/scoredei/ScoreDeiApplication.java
@@ -0,0 +1,13 @@
+package com.example.scoredei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScoreDeiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScoreDeiApplication.class, args);
+    }
+
+}
